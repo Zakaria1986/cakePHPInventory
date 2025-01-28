a5d61f6a56dd6f0fb5310a4974776f891d5bb604
Zak khan <zakaria2012@gmail.com>
2025-01-28 01:27:22 +0000

    Saving phpUnit test in controller. In this test data is not being imported in the database and also data importing directly in the paramers work better than the method dataProvider

--- cms/tests/TestCase/Controller/ProductsControllerTest.php
+++ cms/tests/TestCase/Controller/ProductsControllerTest.php
@@ -16,6 +16,13 @@ use Cake\TestSuite\TestCase;
 class ProductsControllerTest extends TestCase
 {
     use IntegrationTestTrait;
+    // test data 
+    public array $data = [
+        'name' => 'Add new item 1 through controller',
+        'price' => 30.00,
+        'quantity' => 10,
+        'status' => 'in stock'
+    ];
 
     /**
      * Fixtures
@@ -25,6 +32,13 @@ class ProductsControllerTest extends TestCase
     protected array $fixtures = [
         'app.Products',
     ];
+    public function setUp(): void
+    {
+        parent::setUp();
+        $this->enableCsrfToken();
+        $this->enableSecurityToken();
+    }
+
 
     /**
      * Test index method
@@ -34,7 +48,11 @@ class ProductsControllerTest extends TestCase
      */
     public function testIndex(): void
     {
-        $this->markTestIncomplete('Not implemented yet.');
+        $this->get('/products');
+        $this->get('/products/index');
+        $this->get('/products/add');
+        $this->assertResponseOk();
+        $this->assertResponseContains('Products');
     }
 
     /**
@@ -43,21 +61,38 @@ class ProductsControllerTest extends TestCase
      * @return void
      * @uses \App\Controller\ProductsController::view()
      */
-    // public function testView(): void
-    // {
-    //     $this->markTestIncomplete('Not implemented yet.');
-    // }
+    public function testView(): void
+    {
+        $this->get('/products/view/1');
+        $this->assertResponseOk();
+    }
 
     /**
      * Test add method
      *
      * @return void
      * @uses \App\Controller\ProductsController::add()
+     * dataProvider test_data_for_add
      */
-    // public function testAdd(): void
-    // {
-    //     $this->markTestIncomplete('Not implemented yet.');
-    // }
+    public function testAdd(): void
+    {
+        $this->post('/products/add', [
+            'name' => 'Add new item 1 through controller',
+            'price' => 30.00,
+            'quantity' => 10,
+            'status' => 'in stock'
+
+        ]);
+        $this->assertResponseSuccess();
+        $this->assertRedirect(['action' => 'index']);
+
+        // Verify the product was saved in the database
+        $products = $this->getTableLocator()->get('Products');
+        $product = $products->find()->where(['name' => 'Add new item 1 through controller'])->first();
+        $this->assertNotEmpty($product, 'The product saved in the database.');
+        $this->assertEquals(30, $product->price);
+        $this->assertEquals(10, $product->quantity);
+    }
 
     /**
      * Test edit method
@@ -65,10 +100,20 @@ class ProductsControllerTest extends TestCase
      * @return void
      * @uses \App\Controller\ProductsController::edit()
      */
-    // public function testEdit(): void
-    // {
-    //     $this->markTestIncomplete('Not implemented yet.');
-    // }
+    public function testEdit(): void
+    {
+        // seems like datas are being stored in the database
+        $data = [
+            'name' => 'New Product',
+            'price' => 30.00,
+            'quantity' => 10,
+            'status' => 'in stock'
+        ];
+        // getting error on this one
+        $this->put('/products/edit/3', $data);
+        $this->assertResponseSuccess();
+        $this->assertRedirect(['action' => 'index']);
+    }
 
     /**
      * Test delete method
@@ -76,10 +121,35 @@ class ProductsControllerTest extends TestCase
      * @return void
      * @uses \App\Controller\ProductsController::delete()
      */
-    // public function testDelete(): void
+    public function testDelete(): void
+    {
+        $this->delete('/products/delete/12');
+        $this->assertResponseSuccess(); // database is not being connected. This might further need connecting with the database. 
+        $this->assertRedirect(['action' => 'index']);
+
+        $products = $this->getTableLocator()->get('Products');
+        $query = $products->find()->where(['id' => 1]);
+        $this->assertEquals(0, $query->count());
+    }
+
+    // this is data provider for the test however its seem like that is not longer supported as it give depricated warning. 
+
+    // public function test_data_for_add(): array
     // {
-    // $this->markTestIncomplete('Not implemented yet.');
+    //     return [
+
+    //         [
+    //             [
+    //                 'name' => 'Add new item 1 through controller',
+    //                 'price' => 30.00,
+    //                 'quantity' => 10,
+    //                 'status' => 'in stock'
+
+    //             ]
+    //         ]
+    //     ];
     // }
 }
 
+// Command to run when testing the script 
 // vendor/bin/phpunit tests/TestCase/Controller/ProductsControllerTest.php
